980a30de21a2f0c602255a118753b37e4e110948
Kismat Shrestha <kismatds08@gmail.com>
2020-12-14 22:26:06 -0600

    background img

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <HomePage/>
     </div>
   );
 }
